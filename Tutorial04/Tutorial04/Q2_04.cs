fb21b982380d74fa323664d59b23271b5e097d83
KraiNguyen <minhkhoa11062005@gmail.com>
2026-02-03 21:00:57 +0700

    LAB 04 03/02/2026

---
+++ Tutorial04/Tutorial04/Q2_04.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorial04
+{
+    internal class Q2_04
+    {
+    }
+}
---
+++ Tutorial04/Tutorial04/Q5_04.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tutorial04
+{
+    internal class Q5_04
+    {
+    }
+}
